800987272f85903175ab6d0e32558a4e7fc9867f
Gökay Bayram <53994963+gokay98@users.noreply.github.com>
2024-01-03 17:18:53 +0300

    Add files via upload

---
+++ arduino_pot_2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace arduino_pot_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort myport = new SerialPort();
+            myport.BaudRate = 9600;
+            myport.PortName = "COM6";
+            myport.Open();
+            while (true)
+            {
+                String data = myport.ReadLine();
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
